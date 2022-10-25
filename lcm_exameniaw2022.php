932ca8aa240b53b99964d6f35f76d68a04cc6165
Liviu Marian <carataliviu@gmail.com>
2022-10-25 12:16:31 +0200

    primercommit

---
+++ lcm_exameniaw2022.php
@@ -0,0 +1,7 @@
+<?php
+	echo "Liviu Marian"."\n";
+	$fecha = date("d-m-Y-H:m:s")."\n";
+	echo $fecha;
+	echo "El repositorio git es como un directorio de trabajo global en el que podemos subir todo nuestro trabajo para compartirlo con los compañeros. Es un repositorio de versiones.";
+
+?>
